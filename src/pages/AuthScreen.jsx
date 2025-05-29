2814a43624c0e5c4704652034ac8de20dc579771
surendra-19 <surendrachagantipati@gmail.com>
2025-05-29 23:13:41 +0530

    css

--- src/pages/AuthScreen.jsx
+++ src/pages/AuthScreen.jsx
@@ -1,12 +1,15 @@
 import coverImage from '../assets/Cover.jpg'
+import '../index.css'
 export default function AuthScreen() {
     return (
             <>
-                <div>
-                    <div>
-                        <img src={coverImage} />
+                <div className="auth-container">
+                    <div className="image-section">
+                        <img src={coverImage} alt="Cover" />
                     </div>
-                    <div></div>
+                    <div className="content-section">
+                        {/* Your form or content goes here */}
+                    </div>
                 </div>
             </>
     );
