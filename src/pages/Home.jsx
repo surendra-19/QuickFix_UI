4534e5375b2ea35c9a4c6dbcba1f2b9011cd17c4
surendra-19 <surendrachagantipati@gmail.com>
2025-06-10 09:10:03 +0530

    added search functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../index.css'
 import Navbar from '../components/Navbar'
+import Image1 from '../assets/Home_Image1.jpg'
+import SearchForm from '../components/SearchForm'
 
 function Home() {
   return (
     <div className='home-container' >
       <Navbar/>
       <div className="page-container">
+        <SearchForm/>
       </div>
     </div>
   )
