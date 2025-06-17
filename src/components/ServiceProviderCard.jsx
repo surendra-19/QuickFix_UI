9af4fad33a536b1482ce2fdb4e99c51228a98df7
surendra-19 <surendrachagantipati@gmail.com>
2025-06-17 22:17:19 +0530

    service card added

--- src/components/ServiceProviderCard.jsx
+++ src/components/ServiceProviderCard.jsx
@@ -1,12 +1,33 @@
-import '../styles/ServiceProviderCard.css'
+import '../styles/ServiceProviderCard.css';
+import '../index.css';
+import DP from '../assets/demoCard.jpg';
 
 export default function ServiceProviderCard() {
     return (
-        <>
-            <div className="service-card-container">
-                <div></div>
-                <div></div>
+        <div className="service-card-container">
+            <div className="left-section">
+                <img 
+                    src={DP}
+                    alt="service-provider"
+                    className="service-provider-image"
+                />
             </div>
-        </>
+            <div className="right-section">
+                <div className="details-section">
+                    <h2 className="title">Service Provider Name</h2>
+                    <div className="info-group">
+                        <p>Service</p>
+                        <span>4.8</span>
+                    </div>
+                    <div className="info-group1">
+                        <span>Location</span>
+                        <p>120 Orders</p>
+                    </div>
+                </div>
+                <div className="action-section">
+                    <button className="orange-btn">Book Now</button>
+                </div>
+            </div>
+        </div>
     );
 }
